533b298545523331c65994d80f1873f0c62fe04e
marwane akchar <marwaneakchar@gmail.com>
2023-09-03 23:05:58 -0400

    sign up seller page done but not confirming

--- header.php
+++ header.php
@@ -26,5 +26,5 @@
             ?>
             </li>
         </ul>
-    <header>
+            </header>
         
--- join-us.php
+++ join-us.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,61 +8,45 @@
     <link rel="stylesheet" href="./style/join.css">
 </head>
 <body>
- <?php
- include("header.php")
-    ?>
-    <section>
+    <?php  error_reporting(1) ;require("header.php")?>
     <form method="post">
-        <div>
-            <p>name:</p> <input type="text" name="name" id="">
-        </div>
-        <div>
-            <p>Nick Name:</p> <input type="text" name="Nname" id="">
-        </div>
-        <div>
-            <p>phone:</p> <input type="text" name="phone" id="">
-        </div>
-        <div>
-            <p>your store name:</p> <input type="text" name="store" id="">
-        </div>
-        <div>
-            <p>city:</p> <input type="text" name="city" id="">
-        </div>
-        <div>
-            <p>email:</p> <input type="text" name="email" id="">
-        </div>
-        <div>
-            <p>password:</p> <input type="text" name="password" id="">
-        </div>
-        <input type="submit" value="submit">
-        <?php
+    <div><p>name:</p> <input type="text" name="name" id=""></div>
+    <div><p>Nick Name:</p> <input type="text" name="Nname" id=""></div>
+    <div><p>phone:</p> <input type="text" name="phone" id=""></div>
+    <div><p>your store name:</p> <input type="text" name="store" id=""></div>
+    <div><p>city:</p> <input type="text" name="city" id=""></div>
+    <div><p>email:</p> <input type="text" name="email" id=""></div>
+    <div><p>password:</p> <input type="text" name="password" id=""></div>
+    <input type="submit" value="submit">
+    <?php
         if($_SERVER["REQUEST_METHOD"] == "POST"){
             $db = new mysqli("localhost" ,"root","","ecom") ;
-            $email =html_entities($_POST['email']);
-            $name =html_entities($_POST["name"]) ;
-            $Nname = html_entities($_POST['Nname']);
-            $phone =html_entities($_POST['phone']);
-            $store = html_entities($_POST['store']);
-            $city = html_entities($_POST['city']);
-            $email = html_entities($_POST['email']);
-            $pswd = html_entities($_POST['password']);
+            $email =htmlentities($_POST['email']);
+            $name =htmlentities($_POST["name"]) ;
+            $Nname = htmlentities($_POST['Nname']);
+            $phone =htmlentities($_POST['phone']);
+            $store = htmlentities($_POST['store']);
+            $city = htmlentities($_POST['city']);
+            $email = htmlentities($_POST['email']);
+            $pswd = htmlentities($_POST['password']);
+     
             $result = $db->query("select * from seller where email like '$email' or 
-            name like $name' or nickname like '$Nname' or phone like '$phone'
+            name like '$name' or nickname like '$Nname' or phone like '$phone'
              or store like '$store' ") ;
+        
               if ($result->num_rows >0)
               {
-                 echo "sorry we already have a seller with the same informations";
-                 }else{
-                     $DataInsertfb = $db->query("insert into seller
-             (name , nickname , email , phone , ville , password ,date) values
-             ('$name' , '$nickname' , '$email' , '$phone', '$city' , '$pswd' , curdate())        
-                     ");
-                     echo 1 ;
+                 echo "<p style='color:red;'>sorry we already have a seller with the same informations</p>";
+            }else{
+            
+           $DataInsertfb = $db->query("insert into seller
+             (name , nickname , email , phone , ville , password ,date,confirmed) values
+             ('$name' , '$nickname' , '$email' , '$phone', '$city' , '$pswd' , curdate(),false);
+             ");
+        header("location:./confirmSeller.php") ;
             }
         }
     ?>
-    </form>
-    </section>
-</body>
-
+</form>
+    </body>
 </html>
