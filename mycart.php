549958baaae479b611eed0dc94a3a87998edb897
marwane akchar <marwaneakchar@gmail.com>
2023-09-10 11:41:42 -0400

    too lazy to complete

--- mycart.php
+++ mycart.php
@@ -9,15 +9,16 @@
     <link rel="stylesheet" href="./style/mycart.css">
     <script src="https://kit.fontawesome.com/d4341dfe2d.js" crossorigin="anonymous"></script>
 </head>
-
 <body>
     <?php
-    require("header.php")
+    require("header.php");
+    if(!isset($_SESSION["user_id"])){
+        header("Location:./not-found.html");
+    };
         ?>
     <section>
         <input type="button" value="" disabled>
     </section>
     <script src='./scripts/cart.js'></script>
 </body>
-
 </html>
--- seller.php
+++ seller.php
@@ -1,19 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <?php
     session_start() ;
     $db = new mysqli("localhost" ,"root" ,"","ecom") ;
-        if(isset($_SESSION["seller_id"]) && $db->query("select confirmed from seller where id = {$_SESSION['seller_id']}")->fetch_all(MYSQLI_ASSOC)[0]["confirmed"] ){
-            echo 1 ;
-        }else{
-            header("Location:./not-found.html") ;
-        }
+    if(!isset($_SESSION["seller_id"])  ){
+    header("Location:./not-found.html") ;
+}
+    if(!$db->query("select confirmed from seller where id = {$_SESSION['seller_id']}")->fetch_all(MYSQLI_ASSOC)[0]["confirmed"]
+) {header("Location:./not-found.html") ;}
     ?>
+    <header>
+        <ul>
+            <li>
+                Dashboard
+            </li>
+            <li>
+                Stats
+            </li>
+            <li>
+                Profile
+            </li>
+        </ul>
+    </header>
+    <div class="dashboard">
+        <div class="top">
+            <input type="search" id="search">
+        </div>
+        <div class="images">
+        </div>
+        <div class="price">
+
+        </div>
+    </div>
 </body>
+
 </html>
